16a9bc6ab1cc13eeb4792936974c0db20437c1bc
ceduardoos <kaduu1211@gmail.com>
2026-03-11 20:54:08 -0300

    Projeto da apostila 03

---
+++ Apostila03/src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno {
+
+    //Definindo atributos dos alunos
+    String nome;
+    short idade;
+    String nomeCurso;
+    char turma;
+    boolean matriculado;
+    double valorMensalidade;
+    int id;
+
+
+
+}
---
+++ Apostila03/src/Master.java
@@ -0,0 +1,20 @@
+public class Master {
+
+    public static void main(String[] args) {
+        System.out.println("Boa noite!");
+
+        // Declarar variaveis no java
+        short idadeAluno;
+        // Atribuir um valor para a variavel
+        idadeAluno = 32;
+        // Declarar a variavel e atribuir um valor
+        int anoNascimento = 1994;
+        double salario = 16000.50;
+        float altura = 16000.50f;
+        char inicialNome = 'K';
+        boolean gostaAlface = true; // false
+
+
+    }
+
+}
